cda7fc125a3b3776ab867a4b55ce82043a250dba
Isuru Fernando <isuruf@gmail.com>
2021-09-24 10:01:07 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,6 @@
 mkdir build
 cd build
 
-if [[ "$target_platform" == osx-64 ]]; then
-  export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
 cmake ${CMAKE_ARGS} ..
 make -j${CPU_COUNT}
 make install
