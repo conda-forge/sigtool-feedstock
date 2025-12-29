dc6431ca976eea697f51fbc506fbf97d65b1424e
Isuru Fernando <isuruf@gmail.com>
2025-12-29 12:19:23 -0800

    Split package to avoid a binary named codesign

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,3 +6,6 @@ cd build
 cmake ${CMAKE_ARGS} ..
 make -j${CPU_COUNT}
 make install
+
+mv ${PREFIX}/bin/codesign ${PREFIX}/bin/sigtool-codesign
+ln -sf ${PREFIX}/bin/sigtool-codesign ${PREFIX}/bin/codesign
